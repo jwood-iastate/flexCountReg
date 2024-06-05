a2a8f1657271536e552dcdb8e3211a7539ec52fe
Guillermo Basulto <basulto@iastate.edu>
2024-06-05 16:42:27 -0500

    Limit characters and lightly edit example.

--- R/tri.R
+++ R/tri.R
@@ -1,22 +1,28 @@
 #' Triangle Distribution
 #'
-#' These functions provide density, distribution function, quantile function, and random number generation for the Triangle Distribution, specified by its mean, standard deviation, and optional lower and upper bounds.
+#' These functions provide density, distribution function, quantile function,
+#' and random number generation for the Triangle Distribution, specified by its
+#' mean, standard deviation, and optional lower and upper bounds.
 #'
-#' The Triangle Distribution is defined by three points: a (minimum), b (maximum), and c (mode),
-#' where the density is zero outside the interval [a, b], increases linearly from a to c, and
-#' decreases linearly from c to b.
+#' The Triangle Distribution is defined by three points: a (minimum), b
+#' (maximum), and c (mode), where the density is zero outside the interval [a,
+#' b], increases linearly from a to c, and decreases linearly from c to b.
 #'
 #' @param x numeric value or a vector of values.
 #' @param q quantile or a vector of quantiles.
 #' @param p probability or a vector of probabilities.
 #' @param n the number of random numbers to generate.
 #' @param mode numeric value or vector of mode values for the distribution.
-#' @param sigma single value or vector indicating both the positive and negative max differences from the mean (if the difference is the same).
-#' @param upper single value or vector for the upper limit of the distribution (must be used with `lower`).
-#' @param lower single value or vector for the lower limit of the distribution (must be used with `upper`).
+#' @param sigma single value or vector indicating both the positive and negative
+#'   max differences from the mean (if the difference is the same).
+#' @param upper single value or vector for the upper limit of the distribution
+#'   (must be used with `lower`).
+#' @param lower single value or vector for the lower limit of the distribution
+#'   (must be used with `upper`).
 #' @param log logical; if TRUE, probabilities p are given as log(p).
 #' @param log.p logical; if TRUE, probabilities p are given as log(p).
-#' @param lower.tail logical; if TRUE, probabilities p are \eqn{P[X\leq x]} otherwise, \eqn{P[X>x]}.
+#' @param lower.tail logical; if TRUE, probabilities p are \eqn{P[X\leq x]}
+#'   otherwise, \eqn{P[X>x]}.
 #'
 #' @details
 #' \code{dtri} computes the density (PDF) of the Triangle Distribution.
@@ -27,15 +33,15 @@
 #'
 #' \code{rtri} generates random numbers from the Triangle Distribution.
 #'
-#' The mode and standard deviation parameters define the distribution's location and scale,
-#' respectively, while the lower and upper bounds explicitly set the minimum and maximum
-#' values of the distribution.
+#' The mode and standard deviation parameters define the distribution's location
+#' and scale, respectively, while the lower and upper bounds explicitly set the
+#' minimum and maximum values of the distribution.
 #'
 #' @examples
 #' dtri(4, mode=8, upper=13, lower=1)
-#' ptri(c(0,1,2,3,5,7,9,10), mode=3, upper=9, lower=1)
-#' qtri(c(0.1,0.3,0.5,0.9,0.95), mode=3, upper=9, lower=1)
-#' rtri(30, mode=5, sigma=3)
+#' ptri(c(0, 1, 2, 3, 5, 7, 9, 10), mode = 3, upper=9, lower = 1)
+#' qtri(c(0.1, 0.3, 0.5, 0.9, 0.95), mode = 3, upper = 9, lower = 1)
+#' rtri(30, mode = 5, sigma = 3)
 #'
 #' @import stats
 #' @export

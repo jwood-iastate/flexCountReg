2ff357682e1290c82819d67011539afb59e6ff53
Guillermo Basulto <basulto@iastate.edu>
2024-06-06 14:56:39 -0500

    Add test.

---
+++ tests/testthat/test-sichel.R
@@ -0,0 +1,16 @@
+test_that("Sichel regression coefficients", {
+  data("washington_roads")
+  
+  sichel.mod <- sichel(Total_crashes ~ lnaadt + lnlength,
+                       data = washington_roads,
+                       method = "NM",
+                       max.iters = 1000)
+  coefs <- coef(sichel.mod)
+  test_value <- sum(abs(coefs - c(-9.21, 1.12, 0.74, 4.05, 2.5))) < 0.02
+  
+  testthat::expect_true(test_value)
+})
+
+
+
+

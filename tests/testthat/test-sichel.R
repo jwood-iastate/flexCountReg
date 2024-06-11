a18d9c36ad0b688d02630a9580c4044a310c2e86
Guillermo Basulto <basulto@iastate.edu>
2024-06-11 09:48:43 -0500

    Suppress messages for testing.

--- tests/testthat/test-sichel.R
+++ tests/testthat/test-sichel.R
@@ -1,10 +1,12 @@
 test_that("Sichel regression coefficients", {
   data("washington_roads")
   
-  sichel.mod <- sichel(Total_crashes ~ lnaadt + lnlength,
-                       data = washington_roads,
-                       method = "NM",
-                       max.iters = 1000)
+  suppressMessages({
+    sichel.mod <- sichel(Total_crashes ~ lnaadt + lnlength,
+                         data = washington_roads,
+                         method = "NM",
+                         max.iters = 1000)
+  })
   coefs <- coef(sichel.mod)
   test_value <- sum(abs(coefs - c(-9.21, 1.12, 0.74, 4.05, 2.5))) < 0.02
   

eb531009b4e36f4169e0d9b02e241bd2269c8b81
Guillermo Basulto <basulto@iastate.edu>
2024-06-11 10:07:28 -0500

    Add triangular CDF errors.

--- tests/testthat/test-tri.R
+++ tests/testthat/test-tri.R
@@ -1,3 +1,6 @@
+
+## PDF ----
+
 test_that("Triangular PDF", {
   
   ## Riemann sum:
@@ -27,12 +30,41 @@ test_that("Triangular PDF (error 3)", {
   expect_error(dtri(8, mode = 0, upper = 13, lower = 1))
 })
 
+test_that("Triangular CDF", {
+
+  pdf_vals <- ptri(c(0, 1, 3, 9, 10), mode = 3, upper=9, lower = 1)
+  pdf_expected <- c(0, 0, 0.25, 1, 1)
+  out <- all(pdf_expected == pdf_vals)
+  
+  expect_true(out)
+  expect_error(dtri(8, mode = 0, upper = 13, lower = 1))
+})
+
+
+test_that("Triangular CDF (error 1)", {
+  
+  ## Lower > upper
+  expect_error(ptri(8, mode = 8, upper = 13, lower = 14))
+})
+
+test_that("Triangular CDF (error 2)", {
+  
+  ## Mode > upper
+  expect_error(ptri(8, mode = 14, upper = 13, lower = 1))
+})
+
+test_that("Triangular CDF (error 3)", {
+  
+  ## Mode < lower
+  expect_error(ptri(8, mode = 0, upper = 13, lower = 1))
+})
+
 
 # library(testthat)
 # 
 # 
 # plot(\(x) dtri(x, mode=8, upper=13, lower=1), 0, 14)
-# 
 # ptri(c(0, 1, 2, 3, 5, 7, 9, 10), mode = 3, upper=9, lower = 1)
+
 # qtri(c(0.1, 0.3, 0.5, 0.9, 0.95), mode = 3, upper = 9, lower = 1)
 # rtri(30, mode = 5, sigma = 3)

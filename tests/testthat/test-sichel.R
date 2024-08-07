a123f9f55b43ae2b29557a4ea9658752e2666853
Guillermo Basulto <basulto@iastate.edu>
2024-08-07 12:16:54 -0500

    Restore test files.

---
+++ tests/testthat/test-sichel.R
@@ -0,0 +1,14 @@
+test_that("Sichel regression coefficients", {
+  data("washington_roads")
+  
+  suppressMessages({
+    sichel.mod <- sichel(Total_crashes ~ lnaadt + lnlength,
+                         data = washington_roads,
+                         method = "NM",
+                         max.iters = 1000)
+  })
+  coefs <- coef(sichel.mod)
+  test_value <- sum(abs(coefs - c(-9.21, 1.12, 0.74, 4.05, 2.5))) < 0.02
+  
+  testthat::expect_true(test_value)
+})

de59f3bffa9fa7976064938588e784a46320f4ef
Jonathan Wood <89786481+jwood-iastate@users.noreply.github.com>
2024-09-09 20:16:02 -0500

    Update test-pweibreg.R

--- tests/testthat/test-pweibreg.R
+++ tests/testthat/test-pweibreg.R
@@ -1,12 +1,12 @@
 # Test the Poisson-Weibull model with alpha_formula
 test_that("Poisson-Weibull model runs correctly with alpha_formula and sigma_formula", {
   data("washington_roads")
-  model <- pwiebreg(Total_crashes ~ offset(lnaadt) + lnlength,
-                    ndraws = 1500,
+  model <- pwiebreg(Total_crashes ~ lnaadt + lnlength,
+                    ndraws = 10,
                     data = washington_roads,
                     alpha_formula = ~ -1 + lnaadt,
                     sigma_formula = ~ lnaadt,
                     method = 'NM')
   
-  expect_named(model$model$estimate, c("(Intercept)", "lnlength", "ln(alpha):lnaadt", "ln(sigma):(Intercept)", "ln(sigma):lnaadt"))
+  expect_named(model$model$estimate, c("(Intercept)", "lnaadt", "lnlength", "ln(alpha):lnaadt", "ln(sigma):(Intercept)", "ln(sigma):lnaadt"))
 })
